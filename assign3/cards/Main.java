db01687e0580bc180cbdd83d3346b82d60f05981
khueue <khueue@gmail.com>
2010-06-30 17:35:33 +0200

    Clean.

--- assign3/cards/Main.java
+++ assign3/cards/Main.java
@@ -12,13 +12,16 @@ abstract public class Main
     static public void
     main(String[] args)
     {
+        final int width  = 500;
+        final int height = 500;
+
         JFrame frame = new JFrame("Plocka med kort");
         frame.setLocation(25, 25);
-        frame.setSize(500, 500);
+        frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Board board = new Board();
-        board.setSize(500, 500);
+        board.setSize(width, height);
         board.setLayout(null);
         frame.setContentPane(board);
 
