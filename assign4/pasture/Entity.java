1dddd156e119e5c1e69b5dfd7951740494c4ac09
khueue <khueue@gmail.com>
2010-07-20 17:02:34 +0200

    Initial commit for assign4.

---
+++ assign4/pasture/Entity.java
@@ -0,0 +1,31 @@
+/**
+ * OOPJAVA - Assignment 4
+ * Sebastian Lundström (selu7901)
+ */
+
+package pasture;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+/**
+ * This is the superclass of all entities in the pasture simulation
+ * system. This interface <b>must</b> be implemented by all entities
+ * that exist in the simulation of the pasture.
+ */
+public interface Entity
+{
+    public void
+    tick();
+
+    /**
+     * ImageIcon returns the icon of this entity, to be displayed by
+     * the pasture gui.
+     */
+    public ImageIcon
+    getImage();
+
+    public boolean
+    isCompatible(Entity otherEntity);
+}
