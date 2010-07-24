361522ddee9eabd97f177b3f9ff88d55bff2163a
Sebastian Lundström <khueue@gmail.com>
2010-07-25 00:07:02 +0200

    Added intermediate classes.

---
+++ assign4/pasture/ImmobileEntity.java
@@ -0,0 +1,19 @@
+/**
+ * OOPJAVA - Assignment 4
+ * Sebastian Lundström (selu7901)
+ */
+
+package pasture;
+
+import java.util.*;
+import java.awt.Point;
+import javax.swing.ImageIcon;
+
+abstract public class ImmobileEntity extends Entity
+{
+    public
+    ImmobileEntity(Pasture pasture, ImageIcon image)
+    {
+        super(pasture, image);
+    }
+}
